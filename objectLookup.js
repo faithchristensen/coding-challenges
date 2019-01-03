c7480df892dca86080d7e28b1fa380ca9049c0c6
Faith Christensen <faithchristensen111@gmail.com>
2019-01-02 17:16:48 -0800

    Using object properties to look up values for the phonetic alaphabet.

---
+++ objectLookup.js
@@ -0,0 +1,35 @@
+function phoneticLookup(letter) {
+    var phoneticLetters = {
+        a: "Alpha",
+        b: "Bravo",
+        c: "Charlie",
+        d: "Delta",
+        e: "Echo",
+        f: "Foxtrot",
+        g: "Golf",
+        h: "Hotel",
+        i: "India",
+        j: "Juliett",
+        k: "Kilo",
+        l: "Lima",
+        m: "Mike",
+        n: "November",
+        o: "Oscar",
+        p: "Papa",
+        q: "Quebec",
+        r: "Romeo",
+        s: "Sierra",
+        t: "Tango",
+        u: "Uniform",
+        v: "Victor",
+        w: "Whiskey",
+        x: "X-ray",
+        y: "Yankee",
+        z: "Zulu",
+    }
+    return phoneticLetters[letter]
+}
+
+console.log(
+    phoneticLookup('h')
+)
